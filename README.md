9bebe695b2d32e7630cf60798fcf323cf513983d
Christian Wang <christian.gnaw@gmail.com>
2019-10-27 22:37:52 -0400

    Project init and initial docker config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cgi-perl-docker-env
 A simple environment for cgi-perl in docker
+
+Based off of configurations found here https://medium.com/@lojorider/docker-with-cgi-perl-a4558ab6a329
---
+++ environment/hello_world.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use CGI':standard';
+use strict;
+print "Content-type: text/html\n\n";
+
+print "<html> <head>\n";
+print "<title>Hello, world!</title>";
+print "</head>\n";
+print "<body>\n";
+print "<h1>Hello, world!</h1>\n";
+print "</body> </html>\n";
